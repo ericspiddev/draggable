88c30aa24c1dc393466f5a6126294356063246e3
Eric Spidle <ericspidledev@gmail.com>
2025-05-09 22:50:20 -0400

    tmp

--- draggable.js
+++ draggable.js
@@ -1,2 +1,14 @@
 
 console.log("Hello Draggable");
+
+    function dragoverHandler(ev) {
+        ev.preventDefault();
+        ev.dataTransfer.dropEffect = "move";
+    }
+
+    function dropHandler(ev) {
+        ev.preventDefault();
+        // Get the id of the target and add the moved element to the target's DOM
+        const data = ev.dataTransfer.getData("text/plain");
+        ev.target.appendChild(document.getElementById(data));
+    }
